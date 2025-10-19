e8f4038fc606b92fc4edbbcc306c8f72ef438a17
abreoa94-art <abreoa94@gmail.com>
2025-10-19 18:13:11 -0400

    message

--- index.js
+++ index.js
@@ -11,14 +11,24 @@ function searchChange(event){
 let currentMovies = []
 
 
+
+
 //  rendering movies / calling api
- async function renderMovies(searchTerm){
-     const response = await fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=858b2ce0`)
-     const data = await response.json('')
-     currentMovies = data.Search
-     displayMovies(currentMovies)
+async function renderMovies(searchTerm){
+    const response = await fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=858b2ce0`)
+    const data = await response.json('')
+    moviesWrapper.classList += ' movies__loading'
+    if (!movie){
+        movie = await renderMovies
+    }
+
+    moviesWrapper.classList.remove('movies__loading')
+    currentMovies = data.Search
+    displayMovies(currentMovies)
+  
     
 }
+
 //  displaying movies 
 function displayMovies(movieList){
        moviesWrapper.innerHTML = movieList.slice(0, 6).map((movie) => {
@@ -52,3 +62,6 @@ function sortChange(event){
 
     displayMovies(sortedMovies)
 }
+
+// loading 
+
